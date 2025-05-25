40c1468ff81762f458aa0f66f40291812296a480
name <name@name.local>
2025-05-25 22:51:55 +0530

    first commit

---
+++ content.js
@@ -0,0 +1,35 @@
+console.log("🔄 Extension loaded. Watching for 'Stay logged out'...");
+
+let isLogoutClicked = null;
+
+const observer = new MutationObserver((mutations, obs) => {
+  const elements = document.querySelectorAll('button, a, div, span');
+  //console.log("Observing chat gpt for 'Stay logged out'");
+  for (let el of elements) {
+    if (el.textContent.trim() === "Stay logged out") {
+      el.click();
+      console.log("✅ Clicked 'Stay logged out'");
+      obs.disconnect(); // Stop observing
+      //clearTimeout(timeoutId); // Cancel the timeout
+	  isLogoutClicked = true;
+      return;
+    }
+  }
+});
+
+// Start observing changes to the entire document body
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
+
+// Stop observing after 15 seconds if not found
+const timeoutId = setTimeout(() => {
+  observer.disconnect();
+  if(isLogoutClicked){
+      console.log("⏱️ 'Stay logged out' already clicked.");
+	 	  
+  }else{
+      console.log("⏱️ Gave up after 15 seconds. 'Stay logged out' not found.");
+  }
+}, 15000);
